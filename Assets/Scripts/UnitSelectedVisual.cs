0ec23a7cb5b2250b607ca3d2acb124502c48860a
Sergey Kozlov <izumael@bk.ru>
2024-04-20 15:18:50 +0200

    Implement visual for unit selection

---
+++ Assets/Scripts/UnitSelectedVisual.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelectedVisual : MonoBehaviour
+{
+    [SerializeField] private Unit unit;
+    private MeshRenderer meshRenderer;
+    
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Start()
+    {
+        UnitActionSystem.Instance.OnSelectedUnitChanged += UnitActionSystem_OnSelectedUnitChanged;
+        UpateVisual();
+    }
+
+    private void UnitActionSystem_OnSelectedUnitChanged(object sender, System.EventArgs e)
+    {
+        UpateVisual();
+    }
+
+    private void UpateVisual()
+    {
+        if (UnitActionSystem.Instance.GetSelectedUnit() == unit)
+        {
+            meshRenderer.enabled = true;
+        }
+        else
+        {
+            meshRenderer.enabled = false;
+        }
+    }
+}
