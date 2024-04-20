2f1a784c3e3e018d9a12f104b6a20c725177c0a2
Sergey Kozlov <izumael@bk.ru>
2024-04-20 19:36:15 +0200

    Import TMP Essentials, Update Grid System, Implement Grid Movement

---
+++ Assets/Scripts/Grid/GridDebugObject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GridDebugObject : MonoBehaviour
+{
+
+    [SerializeField]  private TextMeshPro textMeshPro;
+    private GridObject gridObject;
+
+    public void SetGridObject(GridObject gridObject)
+    {
+        this.gridObject = gridObject;
+    }
+
+    //private void UpdateVisual()
+    //{
+    //    textMeshPro.text = gridObject.GetPosition();
+    //}
+
+    private void Update()
+    {
+        textMeshPro.text = gridObject.ToString();
+    }
+}
---
+++ Assets/Scripts/Grid/GridObject.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public class GridObject 
+{
+    private GridPosition gridPosition;
+    private GridSystem gridSystem;
+    private List<Unit> unitList;
+
+    public GridObject(GridSystem gridSystem, GridPosition gridPosition)
+    {
+        this.gridSystem = gridSystem;
+        this.gridPosition = gridPosition;
+        unitList = new List<Unit>();
+    }
+
+    //public string GetPosition() { 
+    //    return gridPosition.ToString(); 
+    //}
+
+    public override string ToString()
+    {
+        string unitString = "";
+        foreach (Unit unit in unitList)
+        {
+            unitString += unit.ToString() + "\n";
+        }
+        return gridPosition.ToString() + "\n" + unitString;
+    }
+
+    public void AddUnit(Unit unit)
+    {
+        this.unitList.Add(unit);
+    }
+
+    public void RemoveUnit(Unit unit)
+    {
+        this.unitList.Remove(unit);
+    }
+
+    public List<Unit> GetUnitList()
+    {
+        return this.unitList;
+    }
+}
