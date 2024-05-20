613aeded4561834b5239c0e9d360a6a16eaef725
Sergey Kozlov <izumael@bk.ru>
2024-05-20 21:44:12 +0200

    Refactor Grid System using Generics

--- Assets/Scripts/LevelGrid.cs
+++ Assets/Scripts/LevelGrid.cs
@@ -8,7 +8,7 @@ public class LevelGrid : MonoBehaviour
     public event EventHandler OnAnyUnitMovedGridPosition;
     public static LevelGrid Instance { get; private set; }
     [SerializeField] private Transform gridDebugObjectPrefab;
-    private GridSystem gridSystem;
+    private GridSystem<GridObject> gridSystem;
         
     private void Awake()
     {
@@ -19,7 +19,8 @@ public class LevelGrid : MonoBehaviour
             return;
         }
         Instance = this;
-        gridSystem = new GridSystem(10, 10, 2f);
+        gridSystem = new GridSystem<GridObject>(10, 10, 2f, 
+            (GridSystem<GridObject> g, GridPosition gridPosition) => new GridObject(g, gridPosition));
         gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
     }
 
