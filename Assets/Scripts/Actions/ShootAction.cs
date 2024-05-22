759a5494cbb7799a6aa1aadbb54463df36586aaf
Sergey Kozlov <izumael@bk.ru>
2024-05-22 20:05:21 +0200

    Add screen shake to a shoot action

--- Assets/Scripts/Actions/ShootAction.cs
+++ Assets/Scripts/Actions/ShootAction.cs
@@ -13,6 +13,7 @@ public class ShootAction : BaseAction
         Cooloff,
     }
     private State state;
+    public static event EventHandler<OnShootEventArgs> OnAnyShoot;
     public event EventHandler<OnShootEventArgs> OnShoot;
     public class OnShootEventArgs : EventArgs
     {
@@ -78,6 +79,11 @@ public class ShootAction : BaseAction
 
     private void Shoot()
     {
+        OnAnyShoot?.Invoke(this, new OnShootEventArgs
+        {
+            targetUnit = targetUnit,
+            shootingUnit = unit
+        });
         OnShoot?.Invoke(this, new OnShootEventArgs
         {
             targetUnit = targetUnit,
